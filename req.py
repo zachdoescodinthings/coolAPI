124590668ad026df5d57ebf51c0394225bc33594
Zach <133673574+zachdoescodinthings@users.noreply.github.com>
2023-05-16 11:44:53 +0800

    Update req.py

--- req.py
+++ req.py
@@ -1,28 +1,50 @@
 import requests
 import json
 
-username = "zlyx234248"
-password = "********************" # no peeking ;)
 
-client_id = "30S6ZT3AYYPFYEMWJVMP6YWS"
-client_secret = "**********************************"
-auth_url = "https://www.reddit.com/api/v1/access_token"
-auth_data = {"grant_type": "password", "username": username, "password": password}
-auth_headers = {"User-Agent": "MyBot/0.0.1"}
-auth_response = requests.post(auth_url, auth=(client_id, client_secret), data=auth_data, headers=auth_headers)
-access_token = auth_response.json()["access_token"]
+# Set up OAuth2 authentication with LinkedIn
+client_id = '30S6ZT3AYYPFYEMWJVMP6YWS'
+client_secret = '***************************************'
+redirect_uri = '<your redirect URI here>'
+auth_url = 'https://www.linkedin.com/oauth/v2/authorization'
+access_token_url = 'https://www.linkedin.com/oauth/v2/accessToken'
+scope = 'r_liteprofile'  # Request basic profile information
 
-api_url = "https://oauth.reddit.com/user/{}/about.json".format(username)
-api_headers = {"User-Agent": "MyBot/0.0.1", "Authorization": "bearer {}".format(access_token)}
+# Step 1: Redirect the user to the LinkedIn OAuth2 authentication page
+params = {
+    'response_type': 'code',
+    'client_id': client_id,
+    'redirect_uri': redirect_uri,
+    'scope': scope,
+}
+r = requests.get(auth_url, params=params)
+print(f'Open this URL in your browser: {r.url}')
+auth_code = input('Enter the authorization code from the redirected URL: ')
 
-api_response = requests.get(api_url, headers=api_headers)
+# Step 2: Exchange the authorization code for an access token
+data = {
+    'grant_type': 'authorization_code',
+    'code': auth_code,
+    'redirect_uri': redirect_uri,
+    'client_id': client_id,
+    'client_secret': client_secret,
+}
+r = requests.post(access_token_url, data=data)
+access_token = r.json()['access_token']
 
-if api_response.status_code == 200:
-    api_data = json.loads(api_response.content.decode("utf-8"))
-    karma = api_data["data"]["total_karma"]
-    created_utc = api_data["data"]["created_utc"]
-    print("Username: {}".format(username))
-    print("Total Karma: {}".format(karma))
-    print("Account Created: {}".format(created_utc))
+# Step 3: Use the access token to retrieve some basic profile information
+headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Connection': 'Keep-Alive',
+}
+r = requests.get('https://api.linkedin.com/v2/me?projection=(id,firstName,lastName)', headers=headers)
+if r.ok:
+    data = r.json()
+    print(f'First name: {data["firstName"]["localized"]["en_US"]}')
+    print(f'Last name: {data["lastName"]["localized"]["en_US"]}')
 else:
-    print("Error: {}".format(api_response.status_code))
+    print(f'Request failed with status code {r.status_code}: {r.text}')
+
+    
+    
+
